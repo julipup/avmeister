166070cbdf24b6f865baeabeff0dd8e9bb1a13b0
julipup <dingolediego@gmail.com>
2023-05-21 10:48:15 +0300

    Initial commit

---
+++ src/main/java/org/bluk/avmeister/Avmeister.java
@@ -0,0 +1,17 @@
+package org.bluk.avmeister;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Avmeister extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
